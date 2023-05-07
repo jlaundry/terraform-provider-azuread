1221c0b55fa4512d2dc8dd416f1249cd325602dd
Jed Laundry <jlaundry@jlaundry.com>
2023-05-07 19:13:20 +0000

    fix tests

--- internal/services/applications/application_identifier_uris_resource_test.go
+++ internal/services/applications/application_identifier_uris_resource_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azuread/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azuread/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azuread/internal/clients"
-	"github.com/hashicorp/terraform-provider-azuread/internal/services/applications/parse"
 	"github.com/hashicorp/terraform-provider-azuread/internal/utils"
 )
 
@@ -86,27 +85,26 @@ func (r ApplicationIdentifierUrisResource) Exists(ctx context.Context, clients *
 	client := clients.Applications.ApplicationsClient
 	client.BaseClient.DisableRetries = true
 	defer func() { client.BaseClient.DisableRetries = false }()
-
-	id, err := parse.FederatedIdentityCredentialID(state.ID)
-	if err != nil {
-		return nil, fmt.Errorf("parsing Application Identifier URIs ID: %v", err)
-	}
-
-	credential, status, err := client.GetFederatedIdentityCredential(ctx, id.ObjectId, id.KeyId, odata.Query{})
+	app, status, err := client.Get(ctx, state.ID, odata.Query{})
 	if err != nil {
 		if status == http.StatusNotFound {
-			return nil, fmt.Errorf("Identifier URIs %q for Application with object ID %q does not exist", id.KeyId, id.ObjectId)
+			return nil, fmt.Errorf("Application with object ID %q does not exist", state.ID)
 		}
-		return nil, fmt.Errorf("failed to retrieve Identifier URIs %q for Application with object ID %q: %+v", id.KeyId, id.ObjectId, err)
+		return nil, fmt.Errorf("failed to retrieve Application with object ID %q: %+v", state.ID, err)
 	}
-
-	return utils.Bool(credential != nil), nil
+	return utils.Bool(app.ID() != nil && *app.ID() == state.ID), nil
 }
 
 func (ApplicationIdentifierUrisResource) template(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 resource "azuread_application" "test" {
   display_name = "acctestApplicationIdentifierUris-%[1]d"
+
+  lifecycle {
+    ignore_changes = [
+      identifier_uris,
+    ]
+  }
 }
 `, data.RandomInteger)
 }
@@ -117,9 +115,9 @@ func (r ApplicationIdentifierUrisResource) basic(data acceptance.TestData) strin
 
 resource "azuread_application_identifier_uris" "test" {
   application_object_id = azuread_application.test.object_id
-  identifier_uris       = ["api://${azuread_application.test.id}"]
+  identifier_uris       = ["api://${azuread_application.test.application_id}"]
 }
-`, r.template(data), data.RandomString, data.RandomID)
+`, r.template(data))
 }
 
 func (r ApplicationIdentifierUrisResource) complete(data acceptance.TestData) string {
@@ -132,7 +130,7 @@ data "azuread_domains" "aad_domains" {
 
 resource "azuread_application_identifier_uris" "test" {
   application_object_id = azuread_application.test.object_id
-  identifier_uris       = ["api://${azuread_application.test.id}", "https://example.${data.azuread_domains.aad_domains.domains.0.domain_name}"]
+  identifier_uris       = ["api://${azuread_application.test.application_id}", "https://%s.${data.azuread_domains.aad_domains.domains.0.domain_name}"]
 }
-`, r.template(data), data.RandomString, data.RandomID)
+`, r.template(data), data.RandomString)
 }
